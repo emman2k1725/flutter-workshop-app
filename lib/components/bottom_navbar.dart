1e45df2e97f531c54f4b30413fba59df228ee5c1
Nicole Kaye Cardel <nkcardel@gmail.com>
2024-02-09 19:46:45 +0800

    feature: Bottom Navigation  Bar

---
+++ lib/components/bottom_navbar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workshop_app/screens/screens.dart';
+
+class BottomNavbar extends StatefulWidget {
+  const BottomNavbar({
+    super.key,
+  });
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  int selectedIndex = 0;
+
+  List<Widget> screens = const [
+    HomePage(),
+    SearchPage(),
+    CreatePostPage(),
+    NotificationsPage(),
+    AccountPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(child: screens[selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline_rounded),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline_rounded),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_rounded),
+            label: '',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: const Color(0xFFB8B8B8),
+        onTap: (int index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        elevation: 0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'home_page.dart';
+export 'create_post_page.dart';
+export 'notifications_page.dart';
+export 'search_page.dart';
+export 'account_page.dart';
