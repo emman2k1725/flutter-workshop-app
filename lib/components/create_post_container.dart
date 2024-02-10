081ad5c56c2bdefa28a192aa61ebe28ddcda41f6
Nicole Kaye Cardel <nkcardel@gmail.com>
2024-02-10 20:13:57 +0800

    feature: Create Post Page + Added and Enhanced Components

---
+++ lib/components/create_post_container.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workshop_app/components/components.dart';
+import 'package:flutter_workshop_app/styles/textstyles.dart';
+
+class CreatePostContainer extends StatefulWidget {
+  final TextEditingController controller;
+  const CreatePostContainer({super.key, required this.controller});
+
+  @override
+  State<CreatePostContainer> createState() => _CreatePostContainerState();
+}
+
+class _CreatePostContainerState extends State<CreatePostContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 4.0),
+            child: Icon(
+              Icons.person_pin,
+              size: 30,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'nkcardel',
+                      style: TextStyles.titleText,
+                    ),
+                    widget.controller.text.isNotEmpty &&
+                            widget.controller.text != ''
+                        ? GestureDetector(
+                            onTap: () {
+                              widget.controller.clear();
+                            },
+                            child: const Icon(
+                              Icons.close,
+                              color: Colors.grey,
+                              size: 20,
+                            ),
+                          )
+                        : const SizedBox(),
+                  ],
+                ),
+                const SizedBox(height: 1),
+                CustomTextField(
+                  controller: widget.controller,
+                  type: CustomTextFieldType.createPost,
+                ),
+                const SizedBox(height: 12),
+                const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.photo_library_outlined,
+                      size: 22,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 12),
+                    Icon(
+                      Icons.gif_box_outlined,
+                      size: 22,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 12),
+                    Icon(
+                      Icons.mic_none,
+                      size: 22,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 12),
+                    Icon(
+                      Icons.numbers_rounded,
+                      size: 22,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(width: 12),
+                    Icon(
+                      Icons.poll_outlined,
+                      size: 22,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  'Add to thread',
+                  style: TextStyles.bodyText.copyWith(color: Colors.grey),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workshop_app/styles/textstyles.dart';
 
+enum CustomButtonType {
+  primary,
+  secondary,
+}
+
 class CustomButton extends StatelessWidget {
   final void Function()? onPressed;
   final String? text;
+  final CustomButtonType type;
 
   const CustomButton({
     super.key,
     this.onPressed,
     this.text,
+    this.type = CustomButtonType.primary,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 33,
-      child: ElevatedButton(
-        onPressed: onPressed ?? () {},
-        style: ElevatedButton.styleFrom(
+    ButtonStyle? buttonStyle;
+    Color fontColor = Colors.black;
+
+    switch (type) {
+      case CustomButtonType.primary:
+        buttonStyle = ElevatedButton.styleFrom(
           elevation: 0,
           foregroundColor: Colors.black,
           backgroundColor: Colors.white,
@@ -29,10 +37,30 @@ class CustomButton extends StatelessWidget {
           ),
           tapTargetSize: MaterialTapTargetSize.shrinkWrap,
           splashFactory: NoSplash.splashFactory,
-        ),
+        );
+      case CustomButtonType.secondary:
+        buttonStyle = ElevatedButton.styleFrom(
+          elevation: 0,
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          splashFactory: NoSplash.splashFactory,
+        );
+        fontColor = Colors.white;
+    }
+
+    return SizedBox(
+      height: 33,
+      child: ElevatedButton(
+        onPressed: onPressed ?? () {},
+        style: buttonStyle,
         child: Text(
           text ?? 'Follow',
-          style: TextStyles.bodyText.copyWith(fontWeight: FontWeight.w500),
+          style: TextStyles.bodyText
+              .copyWith(fontWeight: FontWeight.w500, color: fontColor),
         ),
       ),
     );
